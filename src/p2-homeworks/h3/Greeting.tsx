a703f366c76821b42bf1e2c3682664aad75ee78c
Alexey Khazan <khazanalexey@hotmail.com>
2021-04-28 12:33:05 +0300

    hw4

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import s from "./Greeting.module.css"
 import * as events from "events";
-
+import SuperInputText from "../h4/common/c1-SuperInputText/SuperInputText"
+import SuperButton from "../h4/common/c2-SuperButton/SuperButton"
 type GreetingPropsType = {
     name: string // need to fix any
     setNameCallback: (events:React.ChangeEvent<HTMLInputElement>)=>void // need to fix any
@@ -18,10 +19,11 @@ const Greeting: React.FC<GreetingPropsType> = (
 
     return (
         <div className={s.wrapper}>
-            <div className={s.inputclass}><input value={name} onChange={setNameCallback} className={inputClass}/>
-                <span>{error}</span></div>
-
-            <button onClick={addUser}>add</button>
+            <SuperInputText onChange={setNameCallback} value={name} error={error}/>
+            {/*<div className={s.inputclass}><input value={name} onChange={setNameCallback} className={inputClass}/>*/}
+            {/*    <span>{error}</span></div>*/}
+<SuperButton onClick={addUser}>Add</SuperButton>
+            {/*<button onClick={addUser}>add</button>*/}
             <span>{totalUsers}</span>
         </div>
     )
