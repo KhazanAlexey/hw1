22d55d96ea55e8efb5628eaed69508339e654585
Alexey Khazan <khazanalexey@hotmail.com>
2021-05-03 00:27:26 +0300

    hw9 use effect

--- src/p2-homeworks/h9/AlternativeClock.tsx
+++ src/p2-homeworks/h9/AlternativeClock.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 function AlternativeClock() {
+    const [time, settime] = useState<any>()
+const datetoloc=new Date().toLocaleDateString()
+    const tomeloc=new Date().toLocaleTimeString()
+    useEffect(()=>{
+        setInterval(()=>{settime(new Date().toLocaleTimeString())},1000)
 
-
+    },[tomeloc])
     return (
         <div>
-
+            <div>{time}</div>
+            <div>{tomeloc}</div>
+            {datetoloc}
         </div>
     )
 }
